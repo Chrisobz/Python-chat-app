5ef3bf154b3edd3314f50c1da6e86ecffea7fba3
chris <chris@LAPTOP-B53DSNT6>
2023-10-19 22:29:33 +0100

    added templates

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "hjskksk"
 socketio = SocketIO(app)
 
+#route for pages
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return render_template("home.html")
+
 if __name__ == "__main__":
     socketio.run(app, debug=True)
 
